dc8c6d94f977637d7ba5a5b6b34bfd8145d620f9
Wilden Justice <wildenjeffti@gmail.com>
2026-07-08 14:56:02 +0700

    edit main.jsx to address createRoot error

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode, createRoot } from 'react'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
